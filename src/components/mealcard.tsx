1dcfaa2b22d8c1add590a9abbef9e369639cd3ae
rxthew <richardtheumaster@gmail.com>
2022-07-14 00:12:52 +0200

    Set up initial template for MealCard

--- src/components/mealcard.tsx
+++ src/components/mealcard.tsx
@@ -1,26 +1,44 @@
-import React, {useState, useEffect} from 'react'
-
-interface mealData {
- readonly  category : string,
- readonly  name : string, 
- readonly  source: string,
- readonly  tags : string,
- readonly  thumb: string
-
-}
+import {useState, useEffect} from 'react'
 
 const mealDbLink:string = 'https://www.themealdb.com/api/json/v1/1/random.php' 
 
-const singleRandomMeal = async function():Promise<mealData>{
-    const response = await fetch(mealDbLink, {mode:'cors'})
-    const mealData = await response.json()
-    return {
-        category : mealData.meals[0].idMeal,
-        name: mealData.meals[0].idMeal,
-        source: mealData.meals[0].idMeal,
-        tags: mealData.meals[0].idMeal,
-        thumb: mealData.meals[0].idMeal
+
+const MealCard = function():JSX.Element{
+    const [meal,setMeal] = useState<JSX.Element>(<div>Loading...</div>)
+    const SingleRandomMeal  = async function():Promise<void>{
+        const response = await fetch(mealDbLink, {mode:'cors'})
+        const mealData = await response.json()
+        const mealObj = {
+            category : mealData.meals[0].strCategory,
+            name: mealData.meals[0].strMeal,
+            source: mealData.meals[0].strSource,
+            tags: mealData.meals[0].strTags,
+            thumb: mealData.meals[0].strMealThumb
+        }
+        setMeal(
+            <div>
+                <li>{mealObj.category}</li>
+                <li>{mealObj.name}</li>
+                <li>{mealObj.source}</li>
+                <li>{mealObj.tags}</li>
+                <li><img src={mealObj.thumb} alt='meal img'/></li>
+            </div>
+            
+            )
     }
+      
+    useEffect(() => {
+        SingleRandomMeal()
+        
+    },[])
+
+    return (
     
+    <div>      
+    {meal}
+    </div>)
+
 }
+
+export default MealCard
 
