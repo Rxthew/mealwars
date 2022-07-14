ef36c44d27477b6d3c85c5c034095d31ea605fce
rxthew <richardtheumaster@gmail.com>
2022-07-14 20:13:54 +0200

    Apply validation and separate concerns

--- src/components/mealcard.tsx
+++ src/components/mealcard.tsx
@@ -1,34 +1,92 @@
 import {useState, useEffect} from 'react'
 
+
 const mealDbLink:string = 'https://www.themealdb.com/api/json/v1/1/random.php' 
 
+interface mealDbJSON {
+    meals : {
+        0 : {
+            readonly strCategory: string,
+            readonly strMeal: string,
+            readonly strSource: string,
+            readonly strMealThumb: string
+        }
+    }
+    
+}
+
+interface mealData {
+    readonly category: string,
+    readonly name: string,
+    readonly source : string,
+    readonly thumb : string
+
+}
+
+const generateMeal = async function():Promise<mealDbJSON | undefined> {
+    try{
+        const response = await fetch(mealDbLink, {mode:'cors'})
+        const mealData = await response.json()
+        return mealData
+
+    }
+    catch(error){
+        console.log(error)
+        return
+    }
+}
+
+const validateMealData = async function():Promise<mealData | undefined> {
+    for(let count = 0; count < 30; count++){
+        let currentMeal = await generateMeal()
+        if(currentMeal){
+            let mealObj:mealData =  {
+                category : currentMeal.meals[0].strCategory,
+                name: currentMeal.meals[0].strMeal,
+                source: currentMeal.meals[0].strSource,
+                thumb: currentMeal.meals[0].strMealThumb
+            } 
+            let mealValues:string[] = Object.values(mealObj)
+            if(mealValues.every(str => typeof str === 'string' && str.length > 0 )){ 
+                return mealObj
+            
+            }
+        }
+        else{
+            break
+        }
+    }
+    return
+}
 
 const MealCard = function():JSX.Element{
     const [meal,setMeal] = useState<JSX.Element>(<div>Loading...</div>)
-    const SingleRandomMeal  = async function():Promise<void>{
-        const response = await fetch(mealDbLink, {mode:'cors'})
-        const mealData = await response.json()
-        const mealObj = {
-            category : mealData.meals[0].strCategory,
-            name: mealData.meals[0].strMeal,
-            source: mealData.meals[0].strSource,
-            tags: mealData.meals[0].strTags,
-            thumb: mealData.meals[0].strMealThumb
-        }
-        setMeal(
-            <div>
-                <li>{mealObj.category}</li>
-                <li>{mealObj.name}</li>
-                <li>{mealObj.source}</li>
-                <li>{mealObj.tags}</li>
-                <li><img src={mealObj.thumb} alt='meal img'/></li>
-            </div>
-            
+    
+    const singleRandomMeal  = async function():Promise<void>{
+        const mealData = await validateMealData()
+        if(mealData){
+            setMeal(
+                <div>
+                    <li>{mealData.category}</li>
+                    <li>{mealData.name}</li>
+                    <li>{mealData.source}</li>
+                    <li><img src={mealData.thumb} alt='meal img'/></li>
+                </div>
             )
+        }
+        else {
+            setMeal(
+                <div>
+                    No meals available at the moment. Please try refreshing your browser window.
+                </div>
+            )
+        }    
+            
+        
     }
-      
+
     useEffect(() => {
-        SingleRandomMeal()
+        singleRandomMeal()
         
     },[])
 
