66cef0ee989ec907328f774b285d55517a027bbd
rxthew <richardtheumaster@gmail.com>
2022-07-21 23:47:03 +0200

    If meal chosen 5 times display winner
    Ensure everything reset if filter is modified
    
    Add callbacks to set state to update stale refs to props on state change
    
    Add refreshMain so that in case default main meal wins
    
    Context: this is because its reserve data is an empty div with 'new' id
    
    Ensure that if refreshMain doesn't have reserveData then run newRandom
    
    Context: if reserveData undefined default was discarded
    
    Add setTimeOut to refreshMain
    
    Context: setMainCard is async, if newRandom isn't then score add erased

--- src/components/mealwrapper.tsx
+++ src/components/mealwrapper.tsx
@@ -4,6 +4,7 @@ import {MainMealCard, MealCard} from './mealcard'
 import {useState, useEffect} from 'react'
 
 
+
 const mealCategoriesLink:string = 'https://www.themealdb.com/api/json/v1/1/categories.php'
 
 interface mealCategoriesJSON {
@@ -61,15 +62,27 @@ const MealWrapper = function(): JSX.Element{
         populateFilter()
     },[])
     
+
+    const refreshMain = function(reserveData:JSX.Element){
+        if(reserveData){
+            setMainCard( m=> <MainMealCard id={genKey()} cardData={reserveData} filterObject={typeFilter} mainHandleFunction={m.props.mainHandleFunction}/>)  
+
+        }
+        setTimeout(function(){
+            newRandom()
+        },1000)
+        
+    }
+    
     const newRandom = function(){
-        setRandomCard(
-            <MealCard id={genKey()} filterObject={typeFilter} mainHandleFunction={randomCard.props.mainHandleFunction}/>
+        setRandomCard( r =>
+            <MealCard id={genKey()} filterObject={typeFilter} mainHandleFunction={r.props.mainHandleFunction}/>
         )
     }
 
     const newMain = function(reserveData:JSX.Element){
-        setMainCard(
-            <MainMealCard id={genKey()} cardData={reserveData} filterObject={typeFilter} mainHandleFunction={mainCard.props.mainHandleFunction}/>
+        setMainCard( m =>
+            <MainMealCard id={genKey()} cardData={reserveData} filterObject={typeFilter} mainHandleFunction={m.props.mainHandleFunction}/>
         )
 
     }
@@ -78,8 +91,9 @@ const MealWrapper = function(): JSX.Element{
         newRandom()
         newMain(reserveData)
     }
+    
   
-    let [mainCard, setMainCard] = useState<JSX.Element>(<MainMealCard id={genKey()} cardData={<div id='new'></div>} filterObject={typeFilter} mainHandleFunction={newRandom}/>)
+    let [mainCard, setMainCard] = useState<JSX.Element>(<MainMealCard id={genKey()} cardData={<div id='new'></div>} filterObject={typeFilter} mainHandleFunction={refreshMain}/>)
     let [randomCard, setRandomCard] = useState<JSX.Element>(<MealCard id={genKey()} filterObject={typeFilter} mainHandleFunction={promoteToMain}/>)
     let [score, setScore] = useState<number>(0)
 
@@ -92,7 +106,22 @@ const MealWrapper = function(): JSX.Element{
         setScore(0)
     },[mainCard])
 
+    useEffect(()=>{
+        setRandomCard( r =>
+            <MealCard id={genKey()} filterObject={typeFilter} mainHandleFunction={r.props.mainHandleFunction}/>
+        )
+        setMainCard(m => <MainMealCard id={genKey()} cardData={<div id='new'></div>} filterObject={typeFilter} mainHandleFunction={m.props.mainHandleFunction}/>)
 
+    },[typeFilter])
+
+    if(score > 4){ 
+        return(
+            <div>
+                <h2>Looks like you have a winner!</h2>
+                {mainCard.props.cardData}
+            </div>
+        )
+    }
     return (
         <div>
             <CategoryFilter filterObject={typeFilter} setFilterObject={setTypeFilter}/>
